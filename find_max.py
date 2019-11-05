777ae5eb684681e5b4f106fd7c10839ace9343d2
Christian Clauss <cclauss@me.com>
2019-11-05 15:09:07 +0100

    Create find_max.py

---
+++ find_max.py
@@ -0,0 +1,27 @@
+# NguyenU
+
+
+def find_max(nums):
+    """
+    >>> for nums in ([3, 2, 1], [-3, -2, -1], [3, -3, 0], [3.0, 3.1, 2.9]):
+    ...     find_max(nums) == max(nums)
+    True
+    True
+    True
+    True
+    """
+    max_num = nums[0]
+    for x in nums:
+        if x > max_num:
+            max_num = x
+    return max_num
+
+
+def test_find_max():
+    import doctest
+
+    doctest.testmod()
+
+
+if __name__ == "__main__":
+    print(find_max([2, 4, 9, 7, 19, 94, 5]))  # 94
